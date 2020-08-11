1ba71e19ab5b0d38dde697f2c4389811761b30ef
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-08-11 17:59:13 +0545

    Add hint to the programming language

--- src/constant/listOfProgrammingLanguage.js
+++ src/constant/listOfProgrammingLanguage.js
@@ -1,44 +1,225 @@
 export const languages = [
-  "JAVA",
-  "PHP",
-  "C",
-  "PYTHON",
-  "JAVASCRIPT",
-  "VISUAL BASIC .NET",
-  "SQL",
-  "OBJECTIVE-C",
-  "ASSEMBLY LANGUAGE",
-  "MATLAB",
-  "PERL",
-  "OBJECT PASCAL",
-  "FORTRAN",
-  "R",
-  "RUBY",
-  "VISUAL BASIC",
-  "GO",
-  "GROOVY",
-  "SWIFT",
-  "SAS",
-  "D",
-  "LUA",
-  "DART",
-  "COBOL",
-  "SCRATCH",
-  "SCALA",
-  "ABAP",
-  "LISP",
-  "LOGO",
-  "ADA",
-  "TRANSACT-SQL",
-  "PROLOG",
-  "SCHEMA",
-  "KOTLIN",
-  "HASKEL",
-  "JULIA",
-  "POSTSCRIPT",
-  "LADDER LOGIC",
-  "HACK",
-  "ERLANG",
-  "STANDARD ML",
-  "BASH",
+  {
+    name: "JAVA",
+    hint:
+      "is a general purpose programming language that is highly portable due to the fact that compiled code runs on a its virtual machine. Developed by: Sun Microsystems",
+  },
+  {
+    name: "PHP",
+    hint:
+      "is a dynamic programming language primarily designed to create web applications.",
+  },
+  {
+    name: "C",
+    hint:
+      "is a low level programming language that translates efficiently into assembly code. Because of this, it is used to write other high level languages.",
+  },
+  {
+    name: "C++",
+    hint:
+      "https://medium.com/the-open-manuel/top-50-programming-languages-in-the-world-508a119cb714",
+  },
+  {
+    name: "PYTHON",
+    hint:
+      "is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991. It has a design philosophy that emphasizes code readability, notably using significant whitespace.",
+  },
+  {
+    name: "JAVASCRIPT",
+    hint:
+      "is a single-threaded non-blocking asynchronous concurrent language used mainly to create interactivity in web pages.",
+  },
+  {
+    name: "VISUAL BASIC .NET",
+    hint:
+      "is a multi-paradigm, object-oriented programming language, implemented on the .NET Framework. Microsoft launched VB.NET in 2002 as the successor to its original Visual Basic language",
+  },
+  {
+    name: "SQL",
+    hint:
+      "is a domain-specific programming language in that it has a singular purpose: to query a database",
+  },
+  {
+    name: "OBJECTIVE-C",
+    hint:
+      "is a general purpose programming language that was widely used to develop iOS applications before Swift arrived",
+  },
+  {
+    name: "ASSEMBLY LANGUAGE",
+    hint:
+      "is a low-level programming language. Only machine code is lower than assembly code. An assembler compiles assembly code into machine code.",
+  },
+  {
+    name: "MATLAB",
+    hint:
+      "is a computational programming language developed specifically for MathWork’s software",
+  },
+  {
+    name: "PERL",
+    hint:
+      "is a dynamic programming language that borrows features from C,shell script, AWK, and sed",
+  },
+  {
+    name: "OBJECT PASCAL",
+    hint:
+      "is an extension of the Pascal language that was developed at Apple Computer by a team led by Larry Tesler in consultation with Niklaus Wirth, the inventor of Pascal",
+  },
+  {
+    name: "FORTRAN",
+    hint:
+      "is a general-purpose, compiled imperative programming language that is especially suited to numeric computation and scientific computing",
+  },
+  {
+    name: "R",
+    hint:
+      "is a programming language designed for statistical computing and graphics. The language is widely used among statisticians and data miners for developing statistical software and data analysis",
+  },
+  {
+    name: "RUBY",
+    hint: "is a dynamic programming language used to develop web apps quickly",
+  },
+  {
+    name: "VISUAL BASIC",
+    hint:
+      "is an event-driven programming language that was derived from BASIC. It is no longer updated",
+  },
+  {
+    name: "QBASIC",
+    hint:
+      "s an integrated development environment and interpreter for a variety of BASIC programming languages which are based on this language",
+  },
+  {
+    name: "GO",
+    hint:
+      "is a procedural programming language developed by Google with the benefits of a dynamic scripting with the inclusion of a packet management system",
+  },
+  {
+    name: "GROOVY",
+    hint:
+      "is a functional programming language that is compatible with the Java platform due to its similarity in syntax",
+  },
+  {
+    name: "SWIFT",
+    hint:
+      "is a general purpose language that was developed by Apple. It is compatible with Objective-C, which is compatible with C++",
+  },
+  {
+    name: "SAS",
+    hint: "is a computer programming language used for statistical analysis",
+  },
+  {
+    name: "D",
+    hint:
+      "is a general purpose systems and applications programming language that is used to interact directly with the operating system API’s and with hardware",
+  },
+  {
+    name: "LUA",
+    hint:
+      "is a embeddable scripting language that allows for an all in one package of procedural programming, object-oriented programming, functional programming, data-driven programming, and data description",
+  },
+  {
+    name: "DART",
+    hint:
+      "is a general-purpose programming language used to build web, server, desktop, and mobile applications",
+  },
+  {
+    name: "COBOL",
+    hint:
+      "is a compiled computer programming language designed for business applications. It has always been imperative and procedural and has recently become an object oriented language",
+  },
+  {
+    name: "SCRATCH",
+    hint:
+      "is a visual programming language for kids where blocks are used to represent chunks of code",
+  },
+  {
+    name: "SCALA",
+    hint:
+      "is a general purpose programming language that combines object-oriented programming and a functional programming. It’s concise, statically type design addresses the criticisms of Java",
+  },
+  {
+    name: "ABAP",
+    hint:
+      "is a programming language used to develop business solutions using the SAP Application Server in conjunction with the language",
+  },
+  {
+    name: "LISP",
+    hint:
+      "is one of the oldest high-level programming languages. Because of its focus on data structures, Lisp was used for data modeling. Languages like Scheme, Closure, and Common Lisp derived from Lisp",
+  },
+  {
+    name: "LOGO",
+    hint:
+      "is a general purpose language that provides a watered-down version of Lisp for use in an educational setting",
+  },
+  {
+    name: "ADA",
+    hint:
+      "is a structured, statically typed, imperative, and object-oriented high-level computer programming language, extended from Pascal and other languages",
+  },
+  {
+    name: "TRANSACT-SQL",
+    hint:
+      "is a set of programming extensions from Sybase and Microsoft that add several features to the Structured Query Language (SQL), including transaction control, exception and error handling, row processing and declared variables",
+  },
+  {
+    name: "PROLOG",
+    hint:
+      "is a logic programming language associated with artificial intelligence and computational linguistics. It is not algorithmic",
+  },
+  {
+    name: "SCHEMA",
+    hint:
+      "a dialect of Lisp, is a general purpose programming language created at the MIT AI Lab in the 1970’s",
+  },
+  {
+    name: "KOTLIN",
+    hint:
+      "was developed to overcome Java’s major flaws, some of which include raw types and checked exceptions",
+  },
+  {
+    name: "HASKEL",
+    hint:
+      "is a statically typed, purely functional programming language. It’s mainly used for business logic and prototyping",
+  },
+  {
+    name: "JULIA",
+    hint:
+      "is a high level dynamic language designed for numerical analysis and computational science",
+  },
+  {
+    name: "POSTSCRIPT",
+    hint:
+      "is a page description language in the electronic publishing and desktop publishing business. It is a dynamically typed, concatenative programming language and was created at Adobe Systems",
+  },
+  {
+    name: "LADDER LOGIC",
+    hint:
+      "is a programming language that produces a graphical diagram based on the circuit diagrams of relay logic hardware",
+  },
+  {
+    name: "HACK",
+    hint:
+      "is a programming language for the HipHop Virtual Machine, created by Facebook as a dialect of PHP",
+  },
+  {
+    name: "ERLANG",
+    hint:
+      "was developed by Ericsson to aid in the development of software for managing a number of different telecom projects, with the first version being released in 1986, and the first open source release of the language in 1998. It’s most noted for its simple concurrency model",
+  },
+  {
+    name: "STANDARD ML",
+    hint:
+      "is a functional programming language that is widely used by compiler writers and computer science researchers",
+  },
+  {
+    name: "BASH",
+    hint:
+      " is a command language written by Brian Fox and is used within the Bash Shell, which runs Bash scripts",
+  },
+  {
+    name: "RUST",
+    hint:
+      "is a general purpose programming language that was designed to provide a safer alternative to C++",
+  },
 ];
