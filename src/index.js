12244ac508b5083918ca11565e4eae4ffbc7f799
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-08-11 11:42:02 +0545

    Remove other unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,4 +8,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-serviceWorker.unregister();
+
